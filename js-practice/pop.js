fc2143e668e390aacf7c99eeea9994863ff1e940
Koushik Chowdhury <csakoushik@gmail.com>
2025-03-17 14:01:54 +0530

    pop method with keypoints and example

--- js-practice/pop.js
+++ js-practice/pop.js
@@ -6,29 +6,43 @@
 // 4. Pop the last element from `nums = [5, 10, 15]`, store it in `lastNum`, and print both.
 // 5. Check if `obj2 = { key: "value" }` supports the `pop()` method and explain the result.
 
+// 1
 const arr = [100, 200, 300] 
 
 let removedArray = arr.pop(arr)
 console.log(removedArray)
+console.log(arr)
 
+// 2
 const emptyArr =[]
-console.log(emptyArr.pop())
+const res = emptyArr.pop()
+console.log(res)
 
+// 3
 const fruits2 = ["apple", "mango", "grapes"]
 
-let removeElement = fruits2.pop()
+const removeElement = fruits2.pop()
 console.log(removeElement)
+console.log(fruits2)
 
+// 4
 const nums = [5, 10, 15]
+
 let lastNum = nums.pop()
 console.log(lastNum)
+console.log(nums)
 
+// 5
 const student = {
     name:'koushik',
     surname:'chowdhury'
 } 
 
-// student.pop() error not a function
+// KEYPOINTS 
+    // removes the last element from the array
+    // returns the removed element
+    // return undefined if there is no element in the array
+    // not work on objects
 
 
 
