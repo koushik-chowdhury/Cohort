349035bc1d42e16b7b8c4f1a6f4844ede88cb066
Koushik Chowdhury <csakoushik@gmail.com>
2025-03-17 14:10:50 +0530

    push method with keypoints and example

--- js-practice/push.js
+++ js-practice/push.js
@@ -6,29 +6,37 @@
 // 4. Given `obj = { items: ["Item1", "Item2"] }`, push "New Item" into the `items` array and print it.
 // 5. Push `[4, 5, 6]` into `arr1 = [1, 2, 3]` and print the result.
 
-
+// 1
 const fruits = ["apple", "mango"]
 
-fruits.push('orange','banana')
+const newArray = fruits.push('orange','banana')
+console.log(newArray)
 console.log(fruits);
 
+// 2
 const number = []
+
 number.push(10,20,30)
 console.log(number)
 
+// 3
 skills = ['HTML','Css']
-skills.push('Javascript')
 
+skills.push('Javascript')
 console.log(skills)
 
+// 4
 const obj = { items: ["Item1", "Item2"] }
 
 obj.items.push('item3')
 console.log(obj)
 
-
+// 5
 const arr1 = [1,2,3,]
 arr1.push(...[4, 5, 6])
-
 console.log(arr1)
+
+// KEYPOINTS 
+    // push always add element in last index
+    // push return the length of new array
 
