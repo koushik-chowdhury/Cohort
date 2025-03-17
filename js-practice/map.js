a0760405e68fca1bb6cee551bc6e4fe25c7e999a
Koushik Chowdhury <csakoushik@gmail.com>
2025-03-17 13:49:18 +0530

    map method with keypoints and example

--- js-practice/map.js
+++ js-practice/map.js
@@ -6,39 +6,53 @@
 // 4. Extract names from `users = [{name: "John"}, {name: "Alice"}]` using `map()`.
 // 5. Use `map()` to find the length of each string in `strList = ["hello", "world", "JavaScript"]`.
 
+// 1
 const nums3 = [10, 20, 30];
 
 const result = nums3.map((el) => el * 2);
-
 console.log(result);
+console.log(nums3)
 
+// 2
 const celcius = [0, 20, 30];
-
 const fahrenheit = celcius.map(celToFar);
-
 function celToFar(c) {
   return (c * 9) / 5 + 32;
 }
-
 console.log(fahrenheit);
+console.log(celcius)
 
+// 3
 const lowerCaseWords = ["apple", "banana"];
 
 const upperCase = lowerCaseWords.map(changeCase);
 function changeCase(word) {
   return word.toUpperCase();
 }
-
 console.log(upperCase);
+console.log(lowerCaseWords)
 
+// 4
 const users = [{ name: "John" }, { name: "Alice" }];
 
 let names = users.map(({name})=>name)
 console.log(names)
+console.log(users)
 
+// 5
 const strList = ["hello", "world", "JavaScript"]
 
 const len = strList.map((str)=>(
     `${str}:${str.length}`
 ))
 console.log(len)
+console.log(strList)
+
+// KEYPOINTS 
+    // Creates a new array with transformed elements
+    // Does NOT modify the original array.
+
+// Callback function parameters:
+    // value
+    // index
+    // original array
