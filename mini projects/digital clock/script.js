b8226982d3efa1a89f7d6a9225ccc96b8669ce4e
Koushik Chowdhury <csakoushik@gmail.com>
2025-03-25 20:13:49 +0530

    add international time zone

--- mini projects/digital clock/script.js
+++ mini projects/digital clock/script.js
@@ -2,10 +2,14 @@ function updateTime(){
 const time = document.getElementById('time')
 const date = document.getElementById('date')
 
-const now = new Date()
+// const now = new Date()
+const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+const localTime = new Date().toLocaleString("en-US", { timeZone: userTimeZone });
+const now = new Date(localTime)
 const hour = now.getHours() % 12 || 12
 const minuite = now.getMinutes().toString().padStart(2,'0')
 const seconds = now.getSeconds().toString().padStart(2,'0')
+
 
 const options = {
     weekday: "short",
